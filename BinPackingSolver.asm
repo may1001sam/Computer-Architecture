c331ea38d4fe81ce09a2e16b5167247e1a6dc889
Maysam Habbash <126085708+may1001sam@users.noreply.github.com>
2025-03-22 14:47:50 +0200

    file validation

--- BinPackingSolver.asm
+++ BinPackingSolver.asm
@@ -6,7 +6,11 @@
 
 .data
   # data in memory
+  startMessage: .asciiz "Bin Packing Solver\n\n"
+  menu: .asciiz "Choose an operation:\n 1. Enter file name or path.\n 2. Choose Heuristic: FF or BF.\n 3. exit.\n"
   promptFileName: .asciiz "Enter file name:\n"
+  invalidMessage: .asciiz "No Such Operation!\n"
+  invalidFileMsg: .asciiz "Error Opening File.\n"
   fileName: .space 10
   
 .text
@@ -14,7 +18,37 @@
   
   # Main function to run the program
   main:
+    li $v0, 4
+    la $a0, startMessage
+    syscall
     
+   loop:
+   # Display menu
+    li $v0, 4
+    la $a0 menu
+    syscall
+    
+    # read operation
+    li $v0, 5   
+    syscall
+    move $t0, $v0  
+
+   # Switch-case 
+    beq $t0, 1, readFile
+    beq $t0, 2, FForBF
+    beq $t0, 3, exit
+    j invalidOperation  
+    
+   # Handle invalid operations 
+  invalidOperation:
+    li $v0, 4
+    la $a0, invalidMessage
+    syscall
+    j loop  
+  
+  
+  # Function to validate input file
+  readFile:
     # prompt user to input file name
     li $v0, 4
     la $a0, promptFileName
@@ -25,18 +59,27 @@
     li $a1, 10
     li $v0, 8
     syscall
-
-    # return from main
-    li $v0, 10
+    
+    # Open file
+    li $v0, 13
+    la $a0, fileName
     syscall
-   
-  # Function to display menu on screen
-  displayMenu:
+    
+    
+    bltz $v0, invalidFile     
+    # return from function
+    j loop
   
-    jr $ra
-  
-  # Function to validate input file
-  validateFile:
-  
-    jr $ra
+    # File does not exist 
+ invalidFile:
+   li $v0,4
+   la $a0, invalidFileMsg
+   syscall
+               
+ FForBF:
+ 
+    # Exit the program
+ exit:      
+    li $v0, 10
+    syscall          
   
